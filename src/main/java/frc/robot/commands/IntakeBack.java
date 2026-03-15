e82d02257440875d4e7c060ef40eca6f089b7911
Future_Gohan <s1841390@online.houstonisd.org>
2026-03-14 19:03:41 -0500

    Fixed horrendous spacing

--- src/main/java/frc/robot/commands/IntakeBack.java
+++ src/main/java/frc/robot/commands/IntakeBack.java
@@ -1,77 +1,39 @@
 package frc.robot.commands;
 
- 
-
 import edu.wpi.first.wpilibj2.command.Command;
-
 import frc.robot.subsystems.Intake;
-
  
-
 public class IntakeBack extends Command {
 
     private final Intake intake;
 
- 
-
     public IntakeBack(Intake intake) {
-
         this.intake = intake;
 
-     
-
         addRequirements(intake);
-
     }
-
  
-
     @Override
-
     public void initialize() {
-
       intake.stopIntake();
-
     }
 
- 
-
     @Override
-
     public void execute() {
-
         if (!intake.initializing) {
-
           intake.initinitIntake();
-
         }
-
         intake.outitIntake();
-
     }
 
- 
-
     @Override
-
     public boolean isFinished() {
-
         return !intake.initializing;
-
     }
-
- 
 
     @Override
-
     public void end(boolean interrupted) {
-
         return; //intake has already stopped, nothing needs to happen here
-
     }
-
- 
-
-   
 
 }
