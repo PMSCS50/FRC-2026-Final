03d5abe44f333b8730728ad32011f1f59fe57116
Future_Gohan <s1841390@online.houstonisd.org>
2026-03-21 09:31:54 -0500

    tried doing something here

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
-public class    Shooter extends SubsystemBase {
+public class Shooter extends SubsystemBase {
 
     // ************************
     // MOTORS
