fb1b6adc0fd59501d4888449b512016605f1feb5
Future_Gohan <s1841390@online.houstonisd.org>
2026-06-01 10:28:22 -0500

    change directionFlip automatically based on alliance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -79,6 +79,13 @@ public class Robot extends LoggedRobot {
     );
   }
 
+  private void applyAllianceDirFlip() {
+    Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
+    boolean red = alliance == Alliance.Red;
+
+    m_robotContainer.flipDirection(red ? -1 : 1); 
+  }
+
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
@@ -171,6 +178,7 @@ public class Robot extends LoggedRobot {
   public void teleopInit() {
     LimelightHelpers.SetIMUMode(VisionConstants.limelightName, 4);
 
+    applyAllianceDirFlip();
     applyAllianceConfig();
 
     if (m_autonomousCommand != null) {
