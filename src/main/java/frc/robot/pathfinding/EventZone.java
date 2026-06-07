864004a4110efaab421a6b046d8b7e519e84f742
Future-Gohan58 <s1841390@online.houstonisd.org>
2026-06-07 17:27:21 -0500

    EventZones now can also take in registered NamedCommands

--- src/main/java/frc/robot/pathfinding/EventZone.java
+++ src/main/java/frc/robot/pathfinding/EventZone.java
@@ -2,7 +2,9 @@ package frc.robot.pathfinding;
 
 // import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.math.geometry.Translation2d;
+import com.pathplanner.lib.auto.NamedCommands;
 
 /**
  * *A zone where the robot activates a certain command
@@ -11,24 +13,21 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class EventZone extends PathZone {
 
     private final Command command;
-    private final String triggerName;
 
+    //Create an EventZone with a command
     public EventZone(String name,Translation2d min,Translation2d max, Command command) {
         super(name, min, max);
-        this.triggerName = name;
         this.command = command;
+    }
+
+    //Create an EventZone with a registered NamedCommand
+    public EventZone(String name,Translation2d min,Translation2d max, String namedCommand) {
+        super(name, min, max);
+        this.command = (NamedCommands.hasCommand(namedCommand)) ? NamedCommands.getCommand(namedCommand) : Commands.none();
     }
 
     public Command getEvent() {
         return command;
-    }
-
-    public String getTrigger() {
-        return triggerName;
-    }
-
-    public void trigger() {
-        command.schedule();
     }
 
 }
