3304a2ba9608efcc5714c651b1715c98cbf2ed5f
Future-Gohan58 <s1841390@online.houstonisd.org>
2026-06-12 11:56:53 -0500

    Added new audio file in deploy for audio playing. Also added in two new potential audio: Truth Beaty and Hatred (Grey Prince Zote theme) and When Johnny Comes Marching Home Again

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,7 +31,7 @@ public class Robot extends LoggedRobot {
   private final RobotContainer m_robotContainer;
   private boolean allianceConfigApplied = false;
 
-  private final Orchestra m_choralChambersOrchestra = new Orchestra("Choral_Chambers.chrp");
+  private final Orchestra m_choralChambersOrchestra = new Orchestra("audio/Choral_Chambers.chrp");
 
   int[] redTags = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
   int[] blueTags = {17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
