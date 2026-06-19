eb2448401840ea0e2e537de89a5217c411a93149
Future-Gohan58 <s1841390@online.houstonisd.org>
2026-06-19 08:47:45 -0500

    Created two-part diagnostic test to find kCos and kS. First find V1, turn off robot for a few minuted, then find V2 and calc kS and kCos from there. V1, V2, kS and kCos will be logged to AdvantageKit

---
+++ src/main/java/frc/robot/commands/PivotV1Diagnostic.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Pivot;
+
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+//First part of 2-part diagnostic test to find kS and kCos of our pivot
+//MAKE SURE THAT THE PIVOT IS PERFECTLY HORIZONTAL BEFORE BEGINNING THIS TEST
+public class PivotV1Diagnostic extends Command {
+
+    private Pivot pivot;
+    private LoggedNetworkNumber V1 = new LoggedNetworkNumber("Pivot/Diagnostics/V1");
+    private double pivotSpeed = 0;
+    private double lastPivotPosition = -1;
+
+    public PivotV1Diagnostic(Pivot pivot) {
+        this.pivot = pivot;
+    }
+
+    @Override
+    public void initialize() {
+        pivot.resetPivot();
+    }
+
+    @Override
+    public void execute() {
+        pivot.spinPivotDuty(pivotSpeed);
+        if (pivot.getPivotPosition() <= lastPivotPosition) {
+            V1.set(pivotSpeed);
+        }
+        lastPivotPosition = pivot.getPivotPosition();
+        pivotSpeed -= 0.01;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return V1.get() != 0;
+    }
+
+}
