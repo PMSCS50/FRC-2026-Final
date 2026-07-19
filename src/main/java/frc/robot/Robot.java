57fdc7d46b5976de3229831f6db35917280a32bd
Future-Gohan58 <s1841390@online.houstonisd.org>
2026-07-18 20:40:02 -0500

    Roronoa Zoro will be replaced soon possibly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -163,7 +163,7 @@ public class Robot extends LoggedRobot {
     rumbleControllers(0);
 
     if (Constants.currentMode == Constants.simMode) {
-      DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+      DriverStationSim.setAllianceStationId(AllianceStationID.Red2);
     }
 
     // if (!m_orchestra.isPlaying() && allowOrchestra) {
@@ -235,7 +235,7 @@ public class Robot extends LoggedRobot {
   // *Simulation mode
   @Override
   public void simulationInit() {
-    DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+    DriverStationSim.setAllianceStationId(AllianceStationID.Red2);
   }
 
   @Override
