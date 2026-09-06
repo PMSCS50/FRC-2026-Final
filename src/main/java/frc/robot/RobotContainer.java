8aed6b0ac1203694bb9504f3441c720491b3c63b
Future-Gohan58 <s1841390@online.houstonisd.org>
2026-09-06 15:33:21 -0500

    added 4 sword style zoro to more files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -208,10 +208,12 @@ public class RobotContainer {
            driverController.a().whileTrue(new AlignToHub(drivetrain, vision));
         }
 
-        driverController.b().whileTrue(Commands.defer(
-            () -> monkeyDLuffy.goToSelectedWaypoint(),
-            Set.of(drivetrain)
-        ));
+        driverController.b().whileTrue(
+            Commands.defer(
+                () -> monkeyDLuffy.goToSelectedWaypoint(),
+                Set.of(drivetrain)
+            )
+        );
 
         driverController.x().whileTrue(drivetrain.applyRequest(() -> xBrake));
         driverController.y().whileTrue(new InstantCommand(() -> monkeyDLuffy.selectNextWaypoint()));
